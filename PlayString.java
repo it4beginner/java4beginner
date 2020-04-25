d68f8a80a90474bbbb2f0c76be6fe483d499d391
it4beginner <64312131+it4beginner@users.noreply.github.com>
2020-04-25 14:59:16 +0200

    Add files via upload

---
+++ PlayString.java
@@ -0,0 +1,11 @@
+
+public class PlayString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String myFirstName = "James";
+        String myLasttName = "Bond";
+        System.out.println("My name is " + myFirstName + " " + myLasttName);
+	}
+
+}
