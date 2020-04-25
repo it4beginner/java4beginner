a3414954c8b070b6c4eeafb40825c973dad6f60a
it4beginner <64312131+it4beginner@users.noreply.github.com>
2020-04-25 14:43:20 +0200

    Add files via upload

---
+++ PlayMath.java
@@ -0,0 +1,14 @@
+
+public class PlayMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int ii = 5;
+        int jj = 10;
+        int kk = ii + jj;
+        System.out.println("ii is " + ii);
+        System.out.println("jj is " + jj);
+        System.out.println("ii + jj = " + kk);
+	}
+
+}
