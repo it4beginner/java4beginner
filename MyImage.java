dd1cb3423d0cd7471fddcfc997f1efa096427272
it4beginner <64312131+it4beginner@users.noreply.github.com>
2020-05-02 21:56:24 +0200

    Add files via upload

--- MyImage.java
+++ MyImage.java
@@ -6,17 +6,24 @@ import javax.imageio.ImageIO;
 
 public class MyImage {
 	public static void main(String args[]) throws IOException {
-
+        // we place the picture, cat.jpeg, under C:\java
 		BufferedImage bimg = ImageIO.read(new File("C:\\java\\cat.jpg"));
+		// the BufferedImage object provide the methods to get image width and height 
 		int width = bimg.getWidth();
 		int height = bimg.getHeight();
+		// now we go through all pixels
 		for (int ii = 0; ii < width; ii++)
 			for (int jj = 0; jj < height; jj++) {
+				// get color of one pixel
 				int rgb = bimg.getRGB(ii, jj);
+				// get the revert color
 				rgb = 0xffffff - rgb;
+				// set the new color for this pixel 
 				bimg.setRGB(ii, jj, rgb);
 			}
+		// define an output file
 		File output_file = new File("c:\\java\\out.jpg");
+		// write all pixels with new color into this file 
 		ImageIO.write(bimg, "jpg", output_file);
 		System.out.println("Writing complete.");
 	}
