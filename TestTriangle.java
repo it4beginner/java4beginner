e470e27fc74972de5a62ee80f4331ef2fe5ee3c1
it4beginner <64312131+it4beginner@users.noreply.github.com>
2020-05-02 21:11:33 +0200

    Add files via upload

---
+++ TestTriangle.java
@@ -0,0 +1,11 @@
+
+public class TestTriangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Triangle myTriangle = new Triangle(3.0, 4.0, 5.0);
+		System.out.println("myTriangle perimeter " + myTriangle.getPerimeter() + 
+				" myTriangle area " + myTriangle.getArea());
+	}
+
+}
