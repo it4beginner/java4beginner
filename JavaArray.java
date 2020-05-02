788b651e93b953063b7d2d01e7ad454db08daae7
it4beginner <64312131+it4beginner@users.noreply.github.com>
2020-05-02 21:10:57 +0200

    Add files via upload

---
+++ JavaArray.java
@@ -0,0 +1,14 @@
+
+public class JavaArray {
+
+	public static void main(String[] args) {
+        int[] myData = new int[10];
+        for(int ii=0; ii<10; ii++) {
+        	myData[ii] = ii;
+        }
+        for(int ii=0; ii<10; ii++) {
+        	System.out.println(myData[ii]);
+        }
+	}
+
+}
